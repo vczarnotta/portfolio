c671eac104363acd6e8b93b966facca0d2dc40b1
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-03-06 16:07:53 +0100

    om mig

--- src/sections/AboutSection/AboutSection.tsx
+++ src/sections/AboutSection/AboutSection.tsx
@@ -1,7 +1,23 @@
+import s from "./AboutSection.module.css"
+
 export default function AboutSection() {
   return(
-    <section>
+    <section className={s.container}>
       <h2>Om Mig</h2>
+
+      <div className={s.text}>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat deserunt neque obcaecati numquam incidunt dolorem perferendis velit error doloribus eum dignissimos pariatur veritatis, saepe illo quam quas.</p>
+        <p>Neque quae aut laudantium expedita ipsum, minima maxime! Architecto illum enim assumenda reiciendis illo sapiente facere inventore, laudantium autem tempore pariatur doloremque commodi dignissimos, error doloribus, dolorum perferendis cumque quasi sed totam a voluptate et?</p>
+        <p>Dolorem veritatis aliquid placeat assumenda expedita iste necessitatibus. Provident laboriosam excepturi, nostrum aliquid dolores explicabo quisquam porro et, delectus at incidunt blanditiis impedit eligendi voluptatibus ut quasi dicta sunt aliquam dolor quo? Dolores eius repellendus molestiae culpa architecto?</p>
+      </div>
+      
+      <a
+        href="/resume.pdf"
+        download="Veronica_Czarnotta_CV.pdf"
+        className={s.buttonLink}
+      >
+        Ladda ner mitt CV
+      </a>
     </section>
   )
 }
