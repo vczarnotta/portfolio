1ff6c2cf73febb339857aa8728ccc34c223d3c0e
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-03-06 17:11:06 +0100

    färdigheter

--- src/sections/SkillsSection/SkillsSection.tsx
+++ src/sections/SkillsSection/SkillsSection.tsx
@@ -1,7 +1,47 @@
+import SkillCard from "../../components/SkillCard/SkillCard"
+import { LuLayoutTemplate, LuServer, LuWrench, LuCode, LuZap } from "react-icons/lu";
+import s from "./skillsSection.module.css"
+
 export default function SkillsSection() {
   return(
-    <section>
-      <h2>Färdigheter</h2>
+    <section className={s.container}>
+      <h2>Tekniska Färdigheter</h2>
+      <div className={s.skillsWrapper}>
+        <SkillCard
+          variant="lightPink"
+          icon={<LuLayoutTemplate />}
+          title="Frontend"
+          skills={["React", "JavaScript", "TypeScript", "HTML5", "CSS3"]}
+        />
+
+        <SkillCard
+          variant="pink"
+          icon={<LuServer />}
+          title="Backend"
+          skills={["JavaScript", "Node.js"]}
+        />
+
+        <SkillCard
+          variant="blue"
+          icon={<LuWrench />}
+          title="Tools"
+          skills={["Git", "Github", "VS Code"]}
+        />
+
+        <SkillCard
+          variant="lightPink"
+          icon={<LuCode />}
+          title="Core"
+          skills={["JavaScript ES6+", "Async/Await", "Promises"]}
+        />
+
+        <SkillCard
+          variant="pink"
+          icon={<LuZap />}
+          title="Other"
+          skills={["Responsiv Design", "Testing (Jest)", "Agilt arbetssätt", "Webbprestanda"]}
+        />
+      </div>
     </section>
   )
 }
